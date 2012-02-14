d7ed1650ef96c64955455ce1fcc4f1fd17fa62ae
Tim <tim.theodor@gmail.com>
2012-02-14 16:49:29 +0000

    set up basic script structure and loading

---
+++ scripts/loader.js
@@ -0,0 +1,16 @@
+var jewel = {};
+
+window.addEventListener('load', function() {
+	Modernizr.load([
+		{
+			load: [
+				'scripts/sizzle.js',
+				'scripts/dom.js',
+				'scripts/game.js'
+			],
+			complete: function() {
+				console.log('all files loaded');
+			}
+		}
+	]);
+}, false);
