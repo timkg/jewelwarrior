9ae61d5730d37d44e5c7753eb12569ce637245c8
Tim Koch-Grunberg <tim.theodor@gmail.com>
2012-02-14 23:57:57 +0000

    expanded tests, refactored into individual testcases

--- tests/dom-test.js
+++ tests/dom-test.js
@@ -1,4 +1,4 @@
-TestCase('dom.js', {
+TestCase('dom.js - setup', {
 
    'test there is a global jewel object': function() {
 
@@ -10,7 +10,11 @@ TestCase('dom.js', {
 
           assert((jewel && jewel.dom && typeof jewel.dom === 'object') === true);
 
-   },
+   }
+   
+});
+
+TestCase('dom.js - hasClass()', {
    
    'test jewel.dom.hasClass() should return true if an element has a given class': function() {
 
@@ -19,5 +23,87 @@ TestCase('dom.js', {
 
           assert(jewel.dom.hasClass(p, 'test') === true);
 
+    },
+    
+    'test jewel.dom.hasClass() should find a className at the beginning': function() {
+
+              var p = document.createElement('p');
+              p.className = 'test testy';
+
+              assert(jewel.dom.hasClass(p, 'test') === true);
+
+    },
+    
+    'test jewel.dom.hasClass() should find a className at the end': function() {
+
+              var p = document.createElement('p');
+              p.className = 'testy test';
+
+              assert(jewel.dom.hasClass(p, 'test') === true);
+
+    },
+    
+    'test jewel.dom.hasClass() should find a className in the middle': function() {
+
+              var p = document.createElement('p');
+              p.className = 'testy test testoo';
+
+              assert(jewel.dom.hasClass(p, 'test') === true);
+
+        }
+        
+});
+
+TestCase('dom.js - addClass()', {
+
+   'test jewel.dom.addClass() should add a new class to an element': function() {
+
+       var p = document.createElement('p');
+       jewel.dom.addClass(p, 'test');
+
+       assert(jewel.dom.hasClass(p, 'test') === true);
+       
+   },
+   
+   'test jewel.dom.addClass() should non-destructively add a new class to an element with existing classes': function() {
+
+          var p = document.createElement('p');
+          p.className = 'testy'
+          jewel.dom.addClass(p, 'test');
+
+          assert(jewel.dom.hasClass(p, 'test') === true);
+          assert(jewel.dom.hasClass(p, 'testy') === true);
+          assert(jewel.dom.hasClass(p, 'testytest') === false);
+
+      }
+   
+});
+
+TestCase('dom.js - removeClass()', {
+
+   'test jewel.dom.removeClass() should remove a class from an element': function() {
+
+       var p = document.createElement('p');
+       jewel.dom.addClass(p, 'test');
+       
+       assert(jewel.dom.hasClass(p, 'test') === true);
+       
+       jewel.dom.removeClass(p, 'test');
+
+       assert(jewel.dom.hasClass(p, 'test') === false);
+       
+   },
+   
+   'test jewel.dom.removeClass() should leave other classes untouched': function() {
+
+          var p = document.createElement('p');
+          jewel.dom.addClass(p, 'test');
+          jewel.dom.addClass(p, 'testy');
+
+          jewel.dom.removeClass(p, 'test');
+
+          assert(jewel.dom.hasClass(p, 'testy') === true);
+
     }
+   
 });
